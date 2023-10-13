ecf4250bd0395c6f0ea4d6bb8a2e9970cbed1fa1
Antoine Aube <aaube3@gmail.com>
2023-10-13 14:42:09 -0600

    models: user model methods for host/guests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   has_many :viewing_parties, through: :user_viewing_parties
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def parties_i_am_hosting
+    viewing_parties.joins(:user_viewing_parties)
+      .where('user_viewing_parties.host = ? AND user_viewing_parties.user_id = ?', true, self.id)
+      .distinct
+  end
+
+  def parties_i_am_invited_to
+    viewing_parties.joins(:user_viewing_parties)
+    .where('user_viewing_parties.host = ? AND user_viewing_parties.user_id = ?', false, self.id)
+    .distinct
+  end
+  
+  def users_besides_self
+    User.where.not(id: self.id)
+  end
 end
