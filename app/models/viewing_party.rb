a3109e55fec48582137dc826b2153c4be6a4a20c
Antoine Aube <aaube3@gmail.com>
2023-10-13 14:42:52 -0600

    model: viewing methods for dashboard

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -4,4 +4,31 @@ class ViewingParty < ApplicationRecord
   validates :duration, presence: true
   validates :day, presence: true
   validates :view_time, presence: true
+
+
+  def host
+    users.joins(:user_viewing_parties)
+        .select('users.*, user_viewing_parties.host')
+        .find_by('user_viewing_parties.host = ?', true)
+  end
+
+  def party_guests
+    users.joins(:user_viewing_parties)
+        .select('users.*, user_viewing_parties.host')
+        .where('user_viewing_parties.host = ?', false)
+        .distinct
+  end
+
+  def movie
+    find_movie = MovieFacade.new(movie_id)
+    find_movie.movie
+  end
+
+  def movie_title
+    movie.title
+  end
+
+  def movie_image
+    "https://image.tmdb.org/t/p/w500#{movie.image}"
+  end
 end
