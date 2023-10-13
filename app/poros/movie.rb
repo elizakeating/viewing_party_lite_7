8a1520b50229e0899ca8a14f985f6ea82f0edee1
Antoine Aube <aaube3@gmail.com>
2023-10-13 14:43:49 -0600

    poros: add instance variables and draft for formatting methods

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,5 +1,5 @@
 class Movie
-  attr_reader :id, :title, :vote_average, :runtime, :genres, :overview
+  attr_reader :id, :title, :vote_average, :runtime, :genres, :overview, :image
 
   def initialize(data)
     @id = data[:id]
@@ -8,5 +8,10 @@ class Movie
     @runtime = data[:runtime] # will need to accounr for formatting
     @genres = data[:genres] # will need to create format method, also account for sad path as some movies may not have explicit genres
     @overview = data[:overview]
+    @image = data[:backdrop_path]
   end
+
+  # def format_runtime
+  #   " #{@runtime / 60}h #{@runtime % 60}min"
+  # end
 end
